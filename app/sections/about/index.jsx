6a007f7540e69fddb0986eb887009eb90240de69
mallik_dev <mallikk4554@gmail.com>
2024-07-03 17:23:42 +0530

    initial commit

---
+++ app/sections/about/index.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useRef } from "react";
+import { LazyMotion, domAnimation, useInView } from "framer-motion";
+import { HeadingDivider } from "components";
+import { TimeLine } from "./TimeLine";
+
+export function AboutSection() {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
+
+	return (
+		<LazyMotion features={domAnimation}>
+			<section id="about" className="section">
+				<HeadingDivider title="About me" />
+				<div className="pt-10 pb-16 max-w-5xl flex flex-col gap-3">
+					<div
+						tabIndex="0"
+						ref={ref}
+						className="text-xl font-light leading-relaxed"
+						style={{
+							transform: isInView ? "none" : "translateX(-200px)",
+							opacity: isInView ? 1 : 0,
+							transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+						}}
+					>
+						My name is Mallik, and I graduated from Visvesvaraya Technological University.
+						<p className="my-3.5">
+							I have a strong enthusiasm for diving into and mastering new technologies that catch
+							my interest.
+						</p>
+						<p className="my-3.5">
+							In May 2022, I embraced the role of a React developer at a company. Initially
+							uncertain about this career path, I decided to take the leap, and I have not looked
+							back since. Working with React has been an enriching experience, allowing me to
+							continuously develop my skills in frontend development.
+						</p>
+					</div>
+				</div>
+
+				<TimeLine />
+			</section>
+		</LazyMotion>
+	);
+}
